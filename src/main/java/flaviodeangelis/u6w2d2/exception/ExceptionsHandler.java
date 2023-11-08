0055609b0b71ce5e3561bf50933128ab241a3902
flaviodeangelis56 <deangelisflavio@gmail.com>
2023-11-08 16:40:32 +0100

    add-custom-exception

---
+++ src/main/java/flaviodeangelis/u6w2d2/exception/ExceptionsHandler.java
@@ -0,0 +1,32 @@
+package flaviodeangelis.u6w2d2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Date;
+
+@RestControllerAdvice
+public class ExceptionsHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorsPayload handleBadRequest(BadRequestException e) {
+
+        return new ErrorsPayload(e.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(NotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ErrorsPayload handleNotFound(NotFoundException e) {
+        return new ErrorsPayload(e.getMessage(), new Date());
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ErrorsPayload handleGeneric(Exception e) {
+        e.printStackTrace();
+        return new ErrorsPayload("Problema lato server...giuro che lo risolveremo presto", new Date());
+    }
+}
